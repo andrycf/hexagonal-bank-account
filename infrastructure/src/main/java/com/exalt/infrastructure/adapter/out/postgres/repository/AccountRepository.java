d1aec4109ce33269c358b45fdd204a2085f9065c
Christin <andrycf24@gmail.com>
2024-02-01 10:08:59 +0100

    Fix module + refactory

--- infrastructure/src/main/java/com/exalt/infrastructure/adapter/out/postgres/repository/AccountRepository.java
+++ infrastructure/src/main/java/com/exalt/infrastructure/adapter/out/postgres/repository/AccountRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface AccountRepository extends JpaRepository<AccountEntity,Long> {
+public interface AccountRepository extends JpaRepository<AccountEntity,String> {
     Optional<AccountEntity>  findByAccountNumber(String accountNumber);
 }
