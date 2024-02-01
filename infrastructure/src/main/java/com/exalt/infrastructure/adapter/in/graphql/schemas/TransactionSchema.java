38a3e954718c5a7dd7ba1d0822dcc64196f243b4
Christin <andrycf24@gmail.com>
2024-02-01 14:45:28 +0100

    Feature: Add pagination GraphQL

--- infrastructure/src/main/java/com/exalt/infrastructure/adapter/in/graphql/schemas/TransactionSchema.java
+++ infrastructure/src/main/java/com/exalt/infrastructure/adapter/in/graphql/schemas/TransactionSchema.java
@@ -5,11 +5,19 @@ import com.exalt.infrastructure.adapter.in.rest.dto.AccountDTO;
 import com.exalt.infrastructure.adapter.in.rest.dto.TransactionRequest;
 import com.exalt.infrastructure.adapter.in.rest.dto.TransactionResponse;
 import com.exalt.infrastructure.adapter.in.rest.service.TransactionService;
+import com.exalt.infrastructure.adapter.out.postgres.entity.TransactionEntity;
+import com.exalt.infrastructure.adapter.out.postgres.repository.TransactionRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Limit;
+import org.springframework.data.domain.ScrollPosition;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Window;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.graphql.data.query.ScrollSubrange;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -17,6 +25,8 @@ import java.util.List;
 @Controller
 public class TransactionSchema {
     private final TransactionService transactionService;
+    @Autowired
+    private TransactionRepository transactionRepository;
 
     public TransactionSchema(TransactionService transactionService) {
         this.transactionService = transactionService;
@@ -30,6 +40,15 @@ public class TransactionSchema {
     @SchemaMapping(typeName = "Account",field = "getTransactionHistory")
     public List<TransactionResponse> getTransactionHistory(AccountDTO account) {
         return transactionService.checkTransactionHistory(account.accountNumber());
+    }
+
+    @SchemaMapping(typeName = "Account",field = "transactions")
+    public Window<TransactionEntity> getTransactionHistory(AccountDTO account,ScrollSubrange scrollSubrange) {
+        String accountNumber = account.accountNumber();
+        ScrollPosition position = scrollSubrange.position().orElse(ScrollPosition.offset());
+        Limit limit = Limit.of(scrollSubrange.count().orElse(10));
+        Sort sort = Sort.by("accountNumber").ascending();
+        return transactionRepository.findByAccountNumber(accountNumber, position, limit, sort);
     }
 
     @MutationMapping
--- infrastructure/src/main/java/com/exalt/infrastructure/adapter/out/postgres/repository/TransactionRepository.java
+++ infrastructure/src/main/java/com/exalt/infrastructure/adapter/out/postgres/repository/TransactionRepository.java
@@ -1,6 +1,12 @@
 package com.exalt.infrastructure.adapter.out.postgres.repository;
 
 import com.exalt.infrastructure.adapter.out.postgres.entity.TransactionEntity;
+
+
+import org.springframework.data.domain.Limit;
+import org.springframework.data.domain.ScrollPosition;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Window;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +15,5 @@ import java.util.List;
 @Repository
 public interface TransactionRepository  extends JpaRepository<TransactionEntity,Long> {
     List<TransactionEntity> findAllByAccountNumber(String accountNumber);
+    Window<TransactionEntity> findByAccountNumber(String accountNumber,ScrollPosition position, Limit limit, Sort sort);
 }
